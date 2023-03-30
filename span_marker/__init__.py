33c94853f25509bb6e43964d17d28f0267ae7477
Tom Aarsen <Cubiegamedev@gmail.com>
2023-03-30 14:57:47 +0200

    Allow importing TrainingArguments from span_marker

--- span_marker/__init__.py
+++ span_marker/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "0.1.0"
 
-from transformers import AutoConfig, AutoModel
+from transformers import AutoConfig, AutoModel, TrainingArguments
 
 from span_marker.configuration import SpanMarkerConfig
 from span_marker.modeling import SpanMarkerModel
