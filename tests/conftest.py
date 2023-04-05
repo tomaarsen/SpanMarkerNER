03c32f48e4ad16b452896e3e03321c6588d0a094
Tom Aarsen <Cubiegamedev@gmail.com>
2023-04-05 16:58:24 +0200

    Add tiny versions of CoNLL and FewNERD to tests
    This allows for loading and testing with "real" datasets, without having to "Generate train split" for 130.000 samples.

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,19 +27,9 @@ def finetuned_fewnerd_span_marker_model() -> SpanMarkerModel:
 
 @pytest.fixture(scope="session")
 def conll_dataset_dict() -> DatasetDict:
-    train_ds, test_ds = load_dataset("conll2003", split=["train[:2]", "test[:10]"])
-    return DatasetDict({"train": train_ds, "test": test_ds})
+    return DatasetDict.load_from_disk("tests/data/tiny_conll2003")
 
 
 @pytest.fixture(scope="session")
 def fewnwerd_coarse_dataset_dict() -> DatasetDict:
-    train_ds, test_ds = load_dataset("DFKI-SLT/few-nerd", "supervised", split=["train[:2]", "test[:10]"])
-    return DatasetDict({"train": train_ds, "test": test_ds})
-
-
-@pytest.fixture(scope="session")
-def fewnwerd_fine_dataset_dict() -> DatasetDict:
-    train_ds, test_ds = load_dataset("DFKI-SLT/few-nerd", "supervised", split=["train[:2]", "test[:10]"])
-    dataset = DatasetDict({"train": train_ds, "test": test_ds})
-    dataset = dataset.remove_columns("ner_tags")
-    return dataset.rename_column("fine_ner_tags", "ner_tags")
+    return DatasetDict.load_from_disk("tests/data/tiny_fewnerd_super").remove_columns("fine_ner_tags")
