728a034ac66fec2c82e955bc59cdf7fb4a506d93
tomaarsen <tomaarsen@users.noreply.github.com>
2023-04-30 21:51:51 +0000

    deploy: 55f9bdd83438b6f65238025d3711dba306db6d24

---
+++ lib/span_marker/model_card.py
@@ -0,0 +1,38 @@
+MODEL_CARD_TEMPLATE = """
+---
+license: apache-2.0
+library_name: span_marker
+tags:
+- span_marker
+- token-classification
+- ner
+- named-entity-recognition
+pipeline_tag: token-classification
+---
+
+# SpanMarker for Named Entity Recognition
+
+This is a [SpanMarker](https://github.com/tomaarsen/SpanMarkerNER) model that can be used for Named Entity Recognition. \
+In particular, this SpanMarker model uses {encoder_name_or_path} as the underlying encoder.
+
+## Usage
+
+To use this model for inference, first install the `span_marker` library:
+
+```bash
+pip install span_marker
+```
+
+You can then run inference as follows:
+
+```python
+from span_marker import SpanMarkerModel
+
+# Download from Hub and run inference
+model = SpanMarkerModel.from_pretrained("span_marker_model_name")
+# Run inference
+entities = model.predict("Amelia Earhart flew her single engine Lockheed Vega 5B across the Atlantic to Paris.")
+```
+
+See the [SpanMarker](https://github.com/tomaarsen/SpanMarkerNER) repository for documentation and additional information on this model framework.
+"""
