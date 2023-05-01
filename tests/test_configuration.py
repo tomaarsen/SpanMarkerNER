2657e1dba30288c34fc11c0226861538c5e942d2
Tom Aarsen <Cubiegamedev@gmail.com>
2023-05-01 13:30:41 +0200

    Update test according to new HF models

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -14,7 +14,7 @@ def test_config_without_O() -> None:
 def test_config_get_default(finetuned_conll_span_marker_model: SpanMarkerModel) -> None:
     config = finetuned_conll_span_marker_model.config
     # Verify that we can fetch directly from top-level SpanMarkerConfig
-    assert config.get("marker_max_length") == 256
+    assert config.get("marker_max_length") == 128
     # Verify that we can fetch from the underlying encoder, even with multiple options
     assert config.get("hidden_dropout_prob") == 0.1
     assert config.get(["dropout_rate", "hidden_dropout_prob"]) == 0.1
