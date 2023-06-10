0370e1d01c1d61fa4e256a2fe23526003b8a929d
tomaarsen <tomaarsen@users.noreply.github.com>
2023-06-10 13:17:50 +0000

    deploy: 8280f136558078e72247dee8a979b4a21d63ebef

---
+++ lib/span_marker/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "1.0.1"
+
+import logging
+
+from transformers import AutoConfig, AutoModel, TrainingArguments
+
+from span_marker.configuration import SpanMarkerConfig
+from span_marker.modeling import SpanMarkerModel
+from span_marker.trainer import Trainer
+
+AutoConfig.register("span-marker", SpanMarkerConfig)
+AutoModel.register(SpanMarkerConfig, SpanMarkerModel)
+
+logger = logging.getLogger("span_marker")
+logger.setLevel(logging.INFO)
