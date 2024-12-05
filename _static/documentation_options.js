8c304f6c3f9120ff77bc20701090aac97811ddd6
tomaarsen <tomaarsen@users.noreply.github.com>
2024-12-05 10:35:25 +0000

    deploy: cc0ab3ecdffb6e9d35266a0bc6359e687b294106

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.5.1.dev',
+    VERSION: '1.6.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
